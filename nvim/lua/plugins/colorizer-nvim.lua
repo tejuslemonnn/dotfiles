785645dbbe9219f33b9ee557f28d5cb96446daea
tejuslemonnn <tejuslemonnn@gmail.com>
2025-08-01 09:10:21 +0700

    update nvim

---
+++ nvim/lua/plugins/colorizer-nvim.lua
@@ -0,0 +1,15 @@
+-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/colorizer-nvim.lua
+-- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/colorizer-nvim.lua
+
+-- https://github.com/norcalli/nvim-colorizer.lua
+-- This plugin allows me to see the colors of hex code inside files
+
+-- I'm replacing this plugin with mini-hipatterns, installed as a LazyExtra
+
+return {
+  "norcalli/nvim-colorizer.lua",
+  enabled = true,
+  config = function()
+    require("colorizer").setup()
+  end,
+}
